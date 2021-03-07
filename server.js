5b3c05a7a790f5abe5ed328a69984b754564a693
Qi Feng <qi.feng86@gmail.com>
2021-03-07 16:46:09 -0500

    add boiler plate code to server.js file

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
