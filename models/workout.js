6f5f0f9110d73d703e25116f92d2b0482d97cb0e
Qi Feng <qi.feng86@gmail.com>
2021-03-07 22:33:43 -0500

    add html and api routes

--- models/workout.js
+++ models/workout.js
@@ -1,3 +1,4 @@
+//mongodb schema
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const logger = require("morgan");
 const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
+const path = require("path");
 const app = express();
 
 app.use(logger("dev"));
@@ -15,3 +16,60 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+//HTML Routes
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/stats.html"));
+});
+
+//API Routes
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+})
+
+app.get("/api/workouts/range", ({ }, res) => {
+    db.Workout.find({})
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+});
+
+app.post("/api/workouts/", (req, res) => {
+    db.Workout.create(req.body)
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+});
+
+app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate({ _id: req.params.id }, { exercises: req.body })
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
