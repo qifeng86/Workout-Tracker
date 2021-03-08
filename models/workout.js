1afaab01549c9ce2b2b79cc1ba166612855be25b
Qi Feng <qi.feng86@gmail.com>
2021-03-07 22:13:45 -0500

    add mongoose scheme in workout.js

---
+++ models/workout.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    required: true
+                },
+                name: {
+                    type: String,
+                    required: true
+                },
+                distance: {
+                    type: Number
+                },
+                duration: {
+                    type: Number,
+                    required: true
+                },
+                weight: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                }
+
+            }
+        ]
+    });
+
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
