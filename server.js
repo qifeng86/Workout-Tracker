8606bc1e790f9302c9d0c8293f1c3834e82672e1
Qi Feng <qi.feng86@gmail.com>
2021-03-08 22:05:44 -0500

    fix db connection

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 //mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/workouttracker',
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
